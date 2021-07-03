c1ef33e9c20da9f88d1ccbe5f4db1f9eb9e571d8
bladesword <57717364+CrimsonFlame1@users.noreply.github.com>
2021-07-03 14:20:55 +0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,75 @@
+package application;
+
+import java.util.Iterator;
+
+class RingBuffer<T> implements Iterable<T>{
+	
+	private T[] array;
+	private int position = 0;
+	private int numberOfItems = 0;
+	
+	private class RingIterator implements Iterator<T> {
+		
+		private int readPosition = 0;
+
+		@Override
+		public boolean hasNext() {
+			return readPosition < numberOfItems;
+		}
+
+		@Override
+		public T next() {
+			return array[readPosition++];
+		}
+		
+	}
+	
+	@SuppressWarnings("unchecked")
+	public RingBuffer(int size) {
+		array = (T[])new Object[size];
+	}
+	
+	public void add(T obj) {
+		array[position++] = obj;
+		
+		if(position == array.length) {
+			position = 0;
+		}
+		
+		if(++numberOfItems > array.length) {
+			numberOfItems = array.length;
+		}
+	}
+	
+	public int size() {
+		return numberOfItems;
+	}
+	
+	public T get(int index) {
+		return array[index]; 
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		return new RingIterator();
+	}
+	
+}
+
+public class App {
+
+	public static void main(String[] args) {
+		RingBuffer<String> rb = new RingBuffer<>(3);
+		
+		rb.add("saber");
+		rb.add("scatha");
+		rb.add("rem");
+		rb.add("ram");
+		rb.add("emilia");
+		
+		for(var it = rb.iterator(); it.hasNext(); ) {
+			System.out.println(it.next());
+		}
+	}
+
+}
